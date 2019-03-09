ba129936033b9fa9e6acfafba5fe5a4d94c9fb14
Neel <neel.kukreti23@gmail.com>
2019-03-10 03:09:44 +0530

    Added the Add member form except DatePicker

---
+++ app/src/main/java/com/example/fd/ldl/BottomSheetRole.java
@@ -0,0 +1,21 @@
+package com.example.fd.ldl;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BottomSheetRole extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v= inflater.inflate(R.layout.bottom_sheet_layout_role, container, false);
+
+        return v;
+    }
+}
