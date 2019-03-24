ac08d846e7d0eab3bd8fb4d6fb3590cc3b953630
Neel <neel.kukreti23@gmail.com>
2019-03-24 20:44:31 +0530

    Added Calender View

--- app/src/main/java/com/example/fd/ldl/MemberActivity.java
+++ app/src/main/java/com/example/fd/ldl/MemberActivity.java
@@ -1,12 +1,24 @@
 package com.example.fd.ldl;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MemberActivity extends AppCompatActivity {
 
+    private DatePickerDialog.OnDateSetListener dBirth;
+    private DatePickerDialog.OnDateSetListener dJoin;
+    private TextView t_dJoin;
+    private TextView t_dDOB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +28,8 @@ public class MemberActivity extends AppCompatActivity {
         Button bsCollege= findViewById(R.id.btn_colg);
         Button bsRole= findViewById(R.id.btn_role);
         Button bsDay= findViewById(R.id.btn_day);
-
+        t_dJoin= findViewById(R.id.dJoin);
+        t_dDOB= findViewById(R.id.dDOB);
         bsTeam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,5 +68,83 @@ public class MemberActivity extends AppCompatActivity {
                 bsDay.show(getSupportFragmentManager(), "bsDay");
             }
         });
-    }
+
+
+        //CALENDER CODES GO HERE
+
+        t_dJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal= Calendar.getInstance();
+                int year= cal.get(Calendar.YEAR);
+                int month= cal.get(Calendar.MONTH);
+                int day= cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog= new DatePickerDialog(MemberActivity.this,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
+                        dJoin,year,month,day
+
+            );
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+            });
+
+        t_dDOB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal= Calendar.getInstance();
+                int year= cal.get(Calendar.YEAR);
+                int month= cal.get(Calendar.MONTH);
+                int day= cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog= new DatePickerDialog(MemberActivity.this,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
+                        dBirth,year,month,day
+
+                );
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+
+
+        dBirth=  new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                //               Log.d(BATTERY_SERVIC, "ondate :"+month);
+                month=month+1;
+                String jDate= day+"/"+month+"/"+ year;
+
+                t_dDOB.setText(jDate);
+
+            }
+
+
+
+
+        };
+
+       dJoin=  new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+ //               Log.d(BATTERY_SERVIC, "ondate :"+month);
+                month=month+1;
+                String jDate= day+"/"+month+"/"+ year;
+
+                t_dJoin.setText(jDate);
+
+            }
+
+
+
+
+    };
+
+       //CALENDER CODES END HERE
 }
+}
+
